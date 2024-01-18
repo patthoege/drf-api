0c1044aa07f66cd5f411ac57fc2d450e92418e64
Patt Höge <paty.hoge98@gmail.com>
2024-01-18 13:07:56 +0000

    CommentDetailSerializer Challenge

--- comments/models.py
+++ comments/models.py
@@ -7,6 +7,7 @@ class Comment(models.Model):
     """
     Comment model, related to User and Post
     """
+    
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
