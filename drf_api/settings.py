899eb8af8cdcd85afce24cdc693b1a83702a973d
Patt Höge <paty.hoge98@gmail.com>
2024-01-22 12:55:21 +0000

    tready for Deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -39,7 +40,7 @@ REST_FRAMEWORK = {
 
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'restframework.renderers.JSONRenderer'
+        'rest_framework.renderers.JSONRenderer'
     ]
 
 
@@ -61,7 +62,7 @@ SECRET_KEY = 'django-insecure-h=c5#$_o+48wyw%jt1!lx$#waa_0+qtd1s9i_^*w!c5%o_d3_e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-patthoege-drfapi-h8c0hypotei.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-patthoege-drfapi-h8c0hypotei.ws-eu107.gitpod.io'], ['https://drfi-api-a41e37271e42..herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -79,6 +80,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'corsheaders',
     'django.contrib.sites', 
     'allauth', 
     'allauth.account', 
@@ -93,6 +95,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -102,6 +105,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True 
+JWT_AUTH_SAMESITE = 'None' 
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
@@ -127,11 +141,14 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': ({
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        } if 'DEV' in os.environ else dj_database_url.parse(
+            os.environ.get('DATABASE_URL')
+        ))
     }
-}
+print('connected')
 
 
 # Password validation
